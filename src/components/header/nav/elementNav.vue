d65b9f55afbb6b8f42aa235ab17f4e8465900db9
Res1ce <bogratov02@gmail.com>
2023-06-29 00:26:38 +0900

    - Добавлены уведомления

--- src/components/header/nav/elementNav.vue
+++ src/components/header/nav/elementNav.vue
@@ -1,16 +1,22 @@
 <template>
-  <div style="display: flex; flex-direction: column; align-items: center; margin: 0 .7vw; cursor: pointer">
+  <div style="display: flex; flex-direction: column; align-items: center; margin: 0 .7vw; cursor: pointer; position: relative">
     <slot></slot>
     <div class="text">
       {{text}}
+    </div>
+    <div class="notify">
+      {{num}}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+
 defineProps<{
   text: string,
 }>()
+const num = ref(5)
 </script>
 
 <style scoped>
@@ -22,5 +28,21 @@ defineProps<{
   white-space: nowrap;
   letter-spacing: 0.0125vw;
   font-weight: 400;
+}
+.notify{
+  position: absolute;
+  background-color: #ff0000;
+  border-radius: 18px;
+  border: 2px solid white;
+  min-width: 18px;
+  height: 18px;
+  top: -5px;
+  font-size: 10px;
+  line-height: 14px;
+  left: calc(100% - 15px);
+  text-align: center;
+  padding: 0 4px;
+  box-sizing: border-box;
+  color: white;
 }
 </style>
