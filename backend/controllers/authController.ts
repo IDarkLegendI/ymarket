01d0a63f0f31986867c82a58360d2e4645546281
Res1ce <bogratov02@gmail.com>
2023-08-25 01:55:21 +0900

    Часть реализованного этапа авторизации

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,27 +1,40 @@
+import {Request, Response } from 'express';
+import {Logger} from "../modules/logger";
+import {dbRouter} from "../database/db";
+
 export class AuthController {
-	async registration(req, res) {
+	async registration(req: Request, res: Response) {
+		try {
+			const {firstName, lastName, login, password} = req.body
+			console.log(firstName, lastName, login, password)
+			let data = await dbRouter.query(`SELECT id FROM users WHERE login = '${login}'`)
+			if (data[0]) return res.end('Такой аккаунт уже находится в базе данных')
+			data = await dbRouter.query(`INSERT INTO users (firstName, lastName, login, password) VALUES ('${firstName}', '${lastName}', '${login}', '${password}')`)
+			if (!data[0]) return false
+			res.end('Пользователь добавлен в базу данных')
+		}
+		catch (e) {
+			Logger.log("error", e)
+			res.status(400).json()
+		}
+	}
+	async login(req: Request, res: Response) {
 		try {
 
 		}
 		catch (e) {
-
+			Logger.log("error", e)
+			res.status(400).json()
 		}
 	}
-	async login(req, res) {
-		try {
-
-		}
-		catch (e) {
-
-		}
-	}
-	async getUsers(req, res)
+	async getUsers(req: Request, res: Response)
 	{
 		try {
 			res.json('Ivan work!')
 		}
 		catch (e) {
-
+			Logger.log("error", e)
+			res.status(400).json()
 		}
 	}
 }
--- backend/index.ts
+++ backend/index.ts
@@ -2,10 +2,14 @@ import express, { Express, Request, Response } from 'express';
 import {dbRouter} from "./database/db";
 import {Logger} from "./modules/logger";
 import {router} from './routes/authRouter'
+import bodyParser from "body-parser"
+import cors from 'cors'
 
 const app: Express = express()
 const port = 3003
 
+app.use(bodyParser())
+app.use(cors())
 app.use('/auth', router)
 app.get('/api', (req: Request, res: Response) => {
     res.send('BACKEND IS WORKING!')
