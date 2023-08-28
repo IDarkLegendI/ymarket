63749b3805429726341173ca33116559b8cde788
Res1ce <bogratov02@gmail.com>
2023-08-28 18:50:04 +0900

    Начало прикручивания логина регистрации к frontend

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,14 @@
+import $api from "../http";
+import {AxiosResponse} from "axios";
+import {AuthResponse} from "../models/response/authResponse.ts";
+
+export default class AuthService {
+    static async login(login: string, password: string): Promise<AxiosResponse<AuthResponse>>{
+        return $api.post('/login', {login, password})
+    }
+
+    static async registration(firstName: string, lastName: string,
+                              login: string, password: string): Promise<AxiosResponse<AuthResponse>>{
+        return $api.post('/registration', {firstName, lastName, login, password})
+    }
+}
