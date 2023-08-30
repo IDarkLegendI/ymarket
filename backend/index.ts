a05e4931714329cd7d73072b3526c69cf66effb1
DarkLegend <cs-legenda2012@yandex.ru>
2023-08-30 19:21:41 +0300

    renamed router to authRouter

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import {dbRouter} from "./database/db";
 import {Logger} from "./modules/logger";
-import {router} from './routes/authRouter'
+import {authRouter} from './routes/authRouter'
 import bodyParser from "body-parser"
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
@@ -12,7 +12,7 @@ const port = 3003
 app.use(bodyParser())
 app.use(cors())
 app.use(cookieParser())
-app.use('/auth', router)
+app.use('/auth', authRouter)
 
 
 app.get('/api', (req: Request, res: Response) => {
