057c48984cfb541654fb6c0a3a8dccd08b0367e2
DarkLegend <cs-legenda2012@yandex.ru>
2023-08-31 05:48:33 +0300

    searchHeader: добавлена кнопка стереть(когда в поиске больше одного символа)

--- frontend/src/components/header/searchHeader.vue
+++ frontend/src/components/header/searchHeader.vue
@@ -1,19 +1,43 @@
 <template>
   <div class="main">
     <div class="search-box">
-      <div style="width: 100%">
-        <input type="text" @focus="inputFocus" @blur="inputUnFocus" placeholder="Искать товары">
+      <div class="search-input-container">
+        <div style="width: 100%">
+          <input type="text" @focus="inputFocus" @blur="inputUnFocus" placeholder="Искать товары" v-model="searchText">
+        </div>
+        <button class="buttonClear">
+            <div class="buttonClearDiv">
+          </div>
+        </button>
       </div>
-      <Button msg="Найти" hoverClass="buttonFind" />
+      <Button msg="Найти" hoverClass="buttonFind" id="buttonFind" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Button from "../buttons/button.vue";
+import {ref, watch} from "vue";
+const searchText = ref('')
+let documentButtonClearDiv: Element;
+watch(searchText, async (newSearch) => {
+  if(newSearch.length > 0)
+  {
+    documentButtonClearDiv.classList.add('buttonClearDiv_Active')
+  }
+  else {
+    documentButtonClearDiv.classList.remove('buttonClearDiv_Active')
+  }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  documentButtonClearDiv = document.getElementsByClassName('buttonClearDiv')[0]
+});
+
 const inputFocus = () => {
   document.body.classList.add('searchYes')
 }
+document.body.classList.add('searchYes')
 const inputUnFocus = () => {
   document.body.classList.remove('searchYes')
 }
@@ -26,6 +50,13 @@ const inputUnFocus = () => {
   align-items: center;
   z-index: 952;
 }
+
+.search-input-container {
+  display: flex;
+  position: relative;
+  flex-grow: 1;
+}
+
 .search-box{
   display: flex;
   margin-left: 0.63vw;
@@ -56,15 +87,46 @@ const inputUnFocus = () => {
 .search-box input:focus{
   outline: none;
 }
-.search-box Button{
+.search-box #buttonFind{
   border-radius: 0 7px 7px 0;
   padding: 12px 16px;
 }
-.searchYes .search-box Button {
+.searchYes .search-box #buttonFind {
   border: 2px solid white;
 }
 .searchYes .search-box input {
   border: 2px solid white;
   background-color: #fff;
+}
+
+.buttonClear {
+  display: block;
+  z-index: 5;
+  position: absolute;
+  right: 0;
+  height: 100%;
+  padding: 0;
+  border: none;
+  background: transparent;
+}
+
+.buttonClearDiv {
+  width: 38px;
+  height: 100%;
+  background: transparent 50% 50% no-repeat;
+  background-image: url(data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2210%22%20height%3D%2210%22%3E%3Cpolygon%20points%3D%2210%2C0.7%209.3%2C0%205%2C4.3%200.7%2C0%200%2C0.7%204.3%2C5%200%2C9.3%200.7%2C10%205%2C5.7%209.3%2C10%2010%2C9.3%205.7%2C5%22%2F%3E%3C%2Fsvg%3E);
+  background-size: 16px;
+  transition: opacity .1s ease-out;
+  cursor: default;
+  opacity: 0;
+}
+
+.buttonClearDiv_Active {
+  opacity: 0.3;
+  cursor: pointer;
+}
+
+.buttonClear:hover .buttonClearDiv_Active {
+  opacity: 1;
 }
 </style>
