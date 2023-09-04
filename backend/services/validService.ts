1f93d3dae7a177150a3d38bfc7c494a2c007fc84
DarkLegend <cs-legenda2012@yandex.ru>
2023-09-04 17:52:58 +0300

    jsdoc for validService

--- backend/services/validService.ts
+++ backend/services/validService.ts
@@ -1,10 +1,20 @@
 export class ValidService {
+	/**
+	 * @param data - input text for validation
+	 * @return false if text not valid
+	 * @return true if text valid
+	 */
 	static isDataValid(data: string)
 	{
 		const re = /[^A-Z-a-z-0-9]/g;
 		return (!re.test(data))
 	}
 
+	/**
+	 * @param data - input text for validation
+	 * @return false if text not valid
+	 * @return true if text valid
+	 */
 	static isNumberValid(data: string)
 	{
 		const re = /[^0-9]/g;
